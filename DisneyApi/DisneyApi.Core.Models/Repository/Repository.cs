86451fa8926781e903a30c119f65fcad6b9bde1e
ElectroSpyder <prg@live.com.ar>
2021-07-14 00:42:53 -0300

    Creando Repositorios (prueba de genericos)

---
+++ DisneyApi/DisneyApi.Core.Models/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿namespace DisneyApi.Core.Models.Repository
+{
+    using DisneyApi.Core.Models.Context;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected DisneyDBContext DisneyDBContext { get; set; }
+
+        public Repository(DisneyDBContext disneyDBContext)
+        {
+            DisneyDBContext = disneyDBContext;
+        }
+
+        public void Create(T entity)
+        {
+            this.DisneyDBContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.DisneyDBContext.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return this.DisneyDBContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.DisneyDBContext.Set<T>()
+           .Where(expression).AsNoTracking();
+        }
+
+        public void Update(T entity)
+        {
+            this.DisneyDBContext.Set<T>().Update(entity);
+        }
+    }
+}
